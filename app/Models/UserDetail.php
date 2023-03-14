b235e20e264efa867e91ae3edd9db63319d5d89c
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-14 15:43:44 +0100

    relation user and userdetail model

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserDetail extends Model
 {
     use HasFactory;
+
+    // Relazione one to one con UserDetail
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
